53a38219a02203a99805be1191401b966c682966
97hackbrian <brayandurantoconas@gmail.com>
2023-11-25 00:43:23 -0400

    probar embedded system

---
+++ test/segmentImage.py
@@ -0,0 +1,31 @@
+import torch
+import torchvision.transforms as transforms
+import cv2
+
+# 1. Cargar el modelo en PyTorch
+model = torch.load('YoloTrain/runs/segment/train/weights/best.pt')  # Ajusta la ruta según donde hayas guardado tu modelo
+model.eval()
+
+# 2. Preprocesar la entrada
+image = cv2.imread('/home/hackbrian/Descargas/data/dataV1/images/ground/057.jpg')  # Ajusta la ruta según tu imagen
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    # Añade más transformaciones si es necesario (normalización, redimensionamiento, etc.)
+])
+input_image = transform(image).unsqueeze(0)
+
+# 3. Realizar la inferencia
+with torch.no_grad():
+    output = model(input_image)
+
+# 4. Postprocesamiento de resultados
+# (Dependerá del formato de salida específico de tu modelo YOLO)
+
+# 5. Visualización de resultados
+# (Dibuja cuadros delimitadores alrededor de los objetos detectados)
+
+# Muestra la imagen con las detecciones
+cv2.imshow('Detecciones YOLO', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ test/segmentationTorch.py
@@ -0,0 +1,50 @@
+import torch
+import torchvision.transforms as transforms
+import cv2
+import numpy as np
+
+# 1. Cargar el modelo preentrenado
+model = torch.load('ruta/a/tu/modelo.pth')  # Ajusta la ruta según donde hayas guardado tu modelo
+model.eval()
+
+# 2. Configurar la transformación de la imagen
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    # Añade más transformaciones si es necesario (normalización, redimensionamiento, etc.)
+])
+
+# 3. Inicializar la cámara
+cap = cv2.VideoCapture(0)  # Utiliza 0 para la cámara predeterminada, ajusta según sea necesario
+
+while True:
+    # 4. Capturar el cuadro de la cámara
+    ret, frame = cap.read()
+
+    # 5. Preprocesar la imagen
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    input_image = transform(image).unsqueeze(0)
+
+    # 6. Realizar la inferencia
+    with torch.no_grad():
+        output = model(input_image)
+
+    # 7. Procesar la salida (depende de tu modelo y formato de salida)
+
+    # 8. Visualizar la máscara de segmentación
+    mask = output[0].numpy()  # Ajusta según el formato de salida de tu modelo
+    mask = np.argmax(mask, axis=0)  # Puedes necesitar ajustar esta línea según el formato de salida
+
+    # 9. Aplicar la máscara a la imagen original
+    masked_frame = cv2.bitwise_and(frame, frame, mask=(mask > 0).astype(np.uint8))
+
+    # 10. Mostrar la imagen original y la imagen segmentada
+    cv2.imshow('Original', frame)
+    cv2.imshow('Segmentación', masked_frame)
+
+    # 11. Salir del bucle si se presiona la tecla 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 12. Liberar la cámara y cerrar las ventanas
+cap.release()
+cv2.destroyAllWindows()

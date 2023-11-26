081953bde3f87d04f578c78c9471763a800fe3a2
97hackbrian <brayandurantoconas@gmail.com>
2023-11-26 16:42:49 -0400

    Segement

--- YoloTrain/train.bash
+++ YoloTrain/train.bash
@@ -1,1 +1,3 @@
-yolo task=segment mode=train epochs=500 data=dataset.yaml model=yolov8n-seg.pt imgsz=640 batch=2
+/bin/.extreme_cooling start
+yolo task=segment mode=train epochs=66 data=dataset.yaml model=yolov8s-seg.pt imgsz=640 batch=10
+/bin/.extreme_cooling stop

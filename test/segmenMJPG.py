28b5ec0f5f07fd2b92c644219cd59c1fd6a66470
97hackbrian <brayandurantoconas@gmail.com>
2023-11-28 01:47:20 -0400

    SegmenMJPG

--- test/segmenMJPG.py
+++ test/segmenMJPG.py
@@ -4,18 +4,25 @@ import urllib.request
 from ultralytics import YOLO
 
 # URL de la transmisión MJPEG (reemplaza <ip_de_raspberry> con la dirección IP de tu Raspberry Pi)
-url = 'http://<ip_de_raspberry>:8080/?action=stream'
+url = 'http://192.168.100.35:8080/?action=stream'
 
 # Leer nuestro modelo
-model = YOLO("/home/hackbrian/Documentos/train2/runs/segment/train/weights/best.pt")
+model = YOLO("/home/hackbrian/Descargas/best3.pt")
 
 # Configurar el objeto VideoCapture para leer la transmisión MJPEG directamente desde la URL
 cap = cv2.VideoCapture(url)
-
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FPS, 15)
+cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
 # Bucle
 while True:
     # Leer nuestros fotogramas
     ret, frame = cap.read()
+    #frame=frame[:, :, 0]
+    #frame = cv2.resize(frame, (200, 200))
+    frame = cv2.rotate(frame, cv2.ROTATE_180)
+    frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
 
     # Leemos resultados
     resultados = model.predict(frame, imgsz=640, conf=0.8)
